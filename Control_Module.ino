89843a724512579ed2110b9d548989ac49b53e8e
jti20 <44218630+jti20@users.noreply.github.com>
2018-11-18 14:45:21 -0600

    Add files via upload

--- Control_Module.ino
+++ Control_Module.ino
@@ -5,10 +5,11 @@ LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 SoftwareSerial mySerial(2, 3); // RX, TX
 int button = 7;
 int changecount = 0;
+int clearcount = 0;
 
 void setup() {
   lcd.begin(20, 4);  // set up the LCD's number of columns and rows:
-  mySerial.begin(9600);
+  mySerial.begin(1200);
   Serial.begin(9600);
   //Setup button for target advacnce
   pinMode(button, INPUT);
@@ -21,18 +22,28 @@ void loop() {
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Changing Target");
-    while(changecount < 300){
+    while(changecount < 100){
     mySerial.print("1");
     changecount+=1;
     delay(20);
     }
+    while(changecount < 300){
+      lcd.setCursor(0,0);
+      lcd.print("Changing Target");
+      changecount+=1;
+      delay(20);
+    }
     changecount = 0;
+    //Read leftover data to get rid of it
+    if(mySerial.available()>1){
+      mySerial.readString();
+    }
     }
 
   //Check if data is available on HC-12
   if(mySerial.available() > 1){
     String input = mySerial.readString();
-
+    Serial.println(input);
     //Parse incoming string to get individual data
     int stop1 = input.indexOf('!');
     int stop2 = input.indexOf('!',stop1+1);
@@ -45,7 +56,7 @@ void loop() {
     String string4 = input.substring(stop3+1,stop4);
     String string5 = input.substring(stop4+1);
 
-    Serial.println(string5);
+    //Serial.println(string5);
 
     //Add identifiers to data based on order. Also add padding.
     string1 = "Humidity: " + string1 + "%";
@@ -62,23 +73,30 @@ void loop() {
     if(string2.length() < 20){
       int numpad = 20 - string2.length();
       for(int i=0; i<numpad; i++){
-        string1 += ' ';
+        string2 += ' ';
       }
     }
     string3 = "Temp: " + string3 + " F";
     if(string3.length() < 20){
       int numpad = 20 - string3.length();
       for(int i=0; i<numpad; i++){
-        string1 += ' ';
+        string3 += ' ';
       }
     }
     string4 = "Windspeed: " + string4 + " MPH";
     if(string4.length() < 20){
       int numpad = 20 - string4.length();
       for(int i=0; i<numpad; i++){
-        string1 += ' ';
+        string4 += ' ';
       }
     }
+    if(string5.length() < 3){
+      int numpad = 3-string5.length();
+      for(int i=0; i<numpad; i++){
+        string5 += ' ';
+      }
+    }
+  
 
     //Print to LCD Screen
     lcd.setCursor(0,3);
--- Target_Module.ino
+++ Target_Module.ino
@@ -25,12 +25,12 @@ const int Sensor_PIN = A1; // Sensor output voltage
 int changeTarget = 1;      // Received from the controller to index target
 int i = 0;                 // For finding average
 float proximity_AVG = 0.0; // Average of the signals from the sensor
-const int wind_dir = A2;
+const int wind_dir = A2;   // Wind Vane input
 
 BME280 mySensor; //Uses default I2C address 0x77
 
 void setup() {
-  mySerial.begin(9600);
+  mySerial.begin(1200);
   Wire.begin();
   mySensor.setI2CAddress(0x77); //The default for the SparkFun Environmental Combo board is 0x77 (jumper open).
   if(mySensor.beginI2C() == false) Serial.println("Sensor connect failed");
@@ -44,15 +44,18 @@ void loop() {
   //Check if there is a change target command available
   if(mySerial.available() > 1){
     String strinput = mySerial.readString();
+    //Serial.println(strinput);
     if(strinput.charAt(0) == '1'){
+      //Serial.println("Motor");
       //RollMotor
       changeTarget = 1;
       // Turn motor on
       digitalWrite(Motor, HIGH);
+      Serial.println("Motor On");
       // Wait to get off initial black mark
-      delay(1000);
+      delay(3000);
       // Code for sensing stop black mark
-    while (changeTarget == 1){    //While not stop signal detected
+    while (changeTarget == 1){    //While no stop signal detected
       int proximityADC = analogRead(Sensor_PIN);   //Read Sensor
       i += 1;      
       float proximityV = (float)proximityADC * 5.0 / 1023.0; // Convert to 0-5 Volts
@@ -98,6 +101,9 @@ void loop() {
   windspeed = (20.25*windspeed)-8.1;
   windspeed = windspeed*2.237;
   if(windspeed<0){
+    windspeed = 0;
+  }
+  if(windspeed>99){
     windspeed = 0;
   }
 
